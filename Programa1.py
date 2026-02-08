0eb1d73f2fb958ce8cfc085ce9f463805617d9ff
genilsondossantosfilho <genilsondossantosfilho864@gmail.com>
2026-02-08 13:34:54 -0300

    Initial commit

---
+++ Programa1.py
@@ -0,0 +1,22 @@
+word = "abAcaXi muito bom"
+palavra= "     aprendendo python    "
+print(len(word))
+print(word[1::2])
+print(word.count("a",0,3))
+print(word.find("Xi"))
+print(word.replace("Xi","te"))
+print("b" in word)
+print(word.upper())
+print(word.lower())
+print(word[0].upper())
+print(word[0].lower())
+print(word.capitalize())
+print(word.title())
+print(len(palavra))
+print(palavra.__str__())
+
+
+
+
+
+
